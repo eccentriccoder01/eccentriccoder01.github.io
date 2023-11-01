106931e5c48ce02974d5a6406b4488e384a296b4
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-11-01 14:12:06 +0530

    update

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, './src/build')));
 
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './build/index.html'));
+    res.sendFile(path.join(__dirname, './src/build/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
