d88c25fbf534fccb4d68fe897d6e191483bffc99
eccentriccoder01 <sagnikgraviton847@gmail.com>
2023-11-05 00:46:46 +0530

    Update

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const compression = require('compression');
 const path = require('path');
 const app = express();
 
-
 app.use(compression());
 app.use(express.static(path.join(__dirname, './src/build')));
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, './src/build/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const FRONTEND_PORT = process.env.FRONTEND_PORT || 3001; // Change the port here
 
-app.listen(PORT, () => {
-    console.log(`App is running on port ${PORT}`);
+app.listen(FRONTEND_PORT, () => {
+    console.log(`Frontend is running on port ${FRONTEND_PORT}`);
+});
+
+const BACKEND_PORT = process.env.PORT || 3000;
+
+app.listen(BACKEND_PORT, () => {
+    console.log(`Backend is running on port ${BACKEND_PORT}`);
 });
